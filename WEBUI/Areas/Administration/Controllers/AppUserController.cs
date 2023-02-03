0a32b8aa0740c2afe4e1345f812509f0862bd79f
iktueren <iktuerensemih@gmail.com>
2023-02-03 12:55:31 +0300

    AppUser Controller and AdminAuthentication classes created

--- WEBUI/Areas/Administration/Controllers/AppUserController.cs
+++ WEBUI/Areas/Administration/Controllers/AppUserController.cs
@@ -1,13 +1,16 @@
 ﻿using Project.BLL.DesignPatterns.GenericRepository.ConcRepository;
+using Project.ENTITIES.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WEBUI.AuthenticationClasses;
+using WEBUI.VMClasses;
 
 namespace WEBUI.Areas.Administration.Controllers
 {
-   
+    [AdminAuthentication]
     public class AppUserController:Controller
     {
         AppUserRepository _aRep;
@@ -15,6 +18,44 @@ namespace WEBUI.Areas.Administration.Controllers
         public AppUserController()
         {
             _aRep = new AppUserRepository();
+        }
+
+        public ActionResult AppUserList(int? id)
+        {
+            AppUserVM svm = id == null ? new AppUserVM
+            {
+                AppUsers = _aRep.GetActives()
+            } : new AppUserVM { AppUsers = _aRep.Where(x => x.ID == id) };
+            return View(svm);
+        }
+        [HttpPost]
+        public ActionResult AddAppUser(AppUser appuser)
+        {
+            appuser.Active = true;
+            _aRep.Add(appuser);
+            return RedirectToAction("AppUserList");
+        }
+
+        public ActionResult UpdateAppUser(int id)
+        {
+            AppUserVM svm = new AppUserVM
+            {
+                AppUser = _aRep.Find(id)
+            };
+            return View(svm);
+        }
+        [HttpPost]
+        public ActionResult UpdateAppUser(AppUser appuser)
+        {
+            appuser.Active = true;
+            _aRep.Update(appuser);
+            return RedirectToAction("AppUserList");
+        }
+
+        public ActionResult DeleteAppUser(int id)
+        {
+            _aRep.Destroy(_aRep.Find(id));
+            return RedirectToAction("AppUserList");
         }
 
         
---
+++ WEBUI/AuthenticationClasses/AdminAuthentication.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WEBUI.AuthenticationClasses
+{
+    public class AdminAuthentication:AuthorizeAttribute
+    {
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            if (httpContext.Session["admin"] != null)
+            {
+                return true;
+            }
+            httpContext.Response.Redirect("/Shopping/ShoppingList");
+            return false;
+
+            
+        }
+
+    }
+}
