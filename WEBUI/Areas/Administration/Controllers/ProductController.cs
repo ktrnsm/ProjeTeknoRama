158932eba69b10a607956da34a5ad18fe9f63986
iktueren <iktuerensemih@gmail.com>
2023-02-03 18:05:10 +0300

    ProductController class started to build,

--- WEBUI/Areas/Administration/Controllers/ProductController.cs
+++ WEBUI/Areas/Administration/Controllers/ProductController.cs
@@ -1,10 +1,12 @@
 ﻿using Project.BLL.DesignPatterns.GenericRepository.ConcRepository;
+using Project.ENTITIES.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using WEBUI.AuthenticationClasses;
+using WEBUI.Models.Currency;
 using WEBUI.VMClasses;
 
 namespace WEBUI.Areas.Administration.Controllers
@@ -29,6 +31,33 @@ namespace WEBUI.Areas.Administration.Controllers
             ProductVM pvm = new ProductVM
             {
                 Products = _pRep.Where(x=>x.ID==id)
+            };
+            return View(pvm);
+        }
+
+        public ActionResult ProductList(int? id)
+        {
+            ProductVM pvm = new ProductVM
+            {
+                Products = id==null? _pRep.GetActives():_pRep.Where(x=>x.CategoryID==id)
+            };
+
+            Currency c = new Currency();
+            ViewBag.EuroSll = c.EuroSll;
+            ViewBag.EuroBy = c.EuroBy;
+            ViewBag.DolarSll = c.DolarSll;
+            ViewBag.DolarBy = c.DolarBy;
+            return View(pvm);
+        }
+
+        public ActionResult AddProduct()
+        {
+            ProductVM pvm = new ProductVM
+            {
+            Product= new Product(),
+            Categories=_cRep.GetActives(),
+            Suppliers=_sRep.GetActives()
+
             };
             return View(pvm);
         }
