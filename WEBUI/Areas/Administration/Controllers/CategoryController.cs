f7e6ac38f629a67e5a5cb1f454c70ab284581b03
iktueren <iktuerensemih@gmail.com>
2023-02-03 13:17:09 +0300

    Category controller and manager authentication classes  created

---
+++ WEBUI/Areas/Administration/Controllers/CategoryController.cs
@@ -0,0 +1,49 @@
+﻿using Project.BLL.DesignPatterns.GenericRepository.ConcRepository;
+using Project.ENTITIES.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WEBUI.AuthenticationClasses;
+using WEBUI.VMClasses;
+
+namespace WEBUI.Areas.Administration.Controllers
+{
+    [ManagerAuthentication]
+    public class CategoryController: Controller
+    {
+        CategoryRepository _cRep;
+        public CategoryController()
+        {
+            _cRep = new CategoryRepository();
+        }
+
+        public ActionResult CategoryList(int? id)
+        {
+            CategoryVM cvm = id == null ? new CategoryVM
+            {
+                Categories = _cRep.GetActives()
+            } : new CategoryVM { Categories = _cRep.Where(X => X.ID == id) };
+            return View(cvm);           
+
+        }
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddCategory(Category category)
+        {
+            _cRep.Add(category);
+            return RedirectToAction("CategoryList");
+        }
+
+        public ActionResult UpdateCategory(Category category)
+        {
+
+        }
+
+    }
+}
